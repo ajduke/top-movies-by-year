6d0282dc12b873618417b763f5a456fed6abb740
ajduke <ajduke@about.me>
2017-01-29 13:07:24 +0530

    changes in function all

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const list = require(`./year_2016.json`)
 const randomArray = require('unique-random-array')
 
-const all = list
+const all = (year) => list
 const random = randomArray(list)
 
 module.exports = {
